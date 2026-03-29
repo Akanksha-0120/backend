12cd7a9779b82f49ee2c06ad394d33ca019e7b3e
Akanksha-0120 <150513324+Akanksha-0120@users.noreply.github.com>
2026-03-29 15:35:03 +0530

    Add files via upload

---
+++ user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+//**
+// tracking user:
+// password
+// id
+// name
+// email
+// usertype*/
+
+
+const userSchema = new mongoose.Schema({
+     
+    name: {
+        type : String,
+        required : true
+    },
+    userId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password :{
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        lowercase : true,
+        minLength : 10,
+        unique : true
+    },
+    usertype : {
+        type : String,
+        required : true,
+        default : "CUSTOMER",
+        enum : ["CUSTOMER","ADMIN"]
+   }
+},{timestamps : true,versionKey : false})
+
+module.exports = mongoose.model("user",userSchema)
